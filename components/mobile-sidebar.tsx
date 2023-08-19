b2a16faba66987fe707d8345d68223e4f1d06623
shaysadin <shay.sadina29@gmail.com>
2023-08-20 01:25:12 +0300

    fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -8,14 +8,16 @@ import {
 
 import { Sidebar } from './sidebar';
 
-export const MobileSidebar = () => {
+export const MobileSidebar = ({ isPro }: {
+    isPro: boolean;
+}) => {
     return (
         <Sheet>
             <SheetTrigger className='md:hidden pr-4'>
                 <Menu />
             </SheetTrigger>
             <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-                <Sidebar />
+                <Sidebar isPro={isPro} />
             </SheetContent>
         </Sheet>
     )
