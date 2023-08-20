0758dbd4cd3ebefa8b9585e69c48355abd09bef1
shaysadin <shay.sadina29@gmail.com>
2023-08-21 01:25:31 +0300

    change env to local test1

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,6 @@ import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook"],
-  afterAuth(auth, req, evt) {
-    const allowedRoutes = ["/", "/api/webhook", "/sign-in"]; // Add "/sign-in" to allowed routes
-    if (!auth.userId && !allowedRoutes.includes(req.nextUrl.pathname)) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-  },
 });
 
 export const config = {
