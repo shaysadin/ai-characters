8056afc1e8256cc8606f64cf107b00937cd8e267
shaysadin <140343604+shaysadin@users.noreply.github.com>
2023-08-20 11:22:57 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook"],
+  afterAuth(auth, req, evt) {
+    const allowedRoutes = ["/", "/api/webhook", "/sign-in"]; // Add "/sign-in" to allowed routes
+    if (!auth.userId && !allowedRoutes.includes(req.nextUrl.pathname)) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
 });
 
 export const config = {
